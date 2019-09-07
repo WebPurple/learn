10a5ac525822900567366c987586f43076342f50
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-09-07 05:59:53 +0000

    Deploy WebPurple Learn Book to github.com/WebPurple/learn.git:gh-pages

--- gitbook/gitbook-plugin-slides-integration/slides.js
+++
@@ -1,9 +0,0 @@
-require(["gitbook", "jQuery"], function(gitbook, $) {
-  gitbook.events.bind("page.change", function() {
-    $('div.integrate-slides__container').each(function(slide) {
-      var width = $(this).width();
-      var height = width / 4 * 2.5;
-      $(this).height(height);
-    });
-  });
-});
---
+++ styles-b070b1419980ad7dbab9.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[7],{229:function(n,w,o){}}]);
+//# sourceMappingURL=styles-b070b1419980ad7dbab9.js.map
